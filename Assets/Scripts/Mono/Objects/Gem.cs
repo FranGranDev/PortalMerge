dfed6975f38be2bd99715e575ce4c29a4bef8c80
FranGranDev <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>
2021-10-13 01:21:34 +0300

    Gem model added

---
+++ Assets/Scripts/Mono/Objects/Gem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gem : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Cube")
+        {
+
+        }
+    }
+}
